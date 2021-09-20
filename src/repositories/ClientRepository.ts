9ee161676577da2e0d6341cb438c2fddb2a8546f
Pablo Martins <pablodvmartins@gmail.com>
2021-09-20 20:08:50 -0300

    ✨ Create ClientRepository an method findAll

---
+++ src/repositories/ClientRepository.ts
@@ -0,0 +1,12 @@
+import Client from '../models/Client';
+
+export default class ClientRepository {
+  private clients: Array<Client>;
+
+  constructor() {
+    this.clients = [];
+  }
+
+  public findAll(): Array<Client> {
+    return this.clients;
+  }
